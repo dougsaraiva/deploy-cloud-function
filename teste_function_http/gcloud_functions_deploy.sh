1cf2f7799c139c34ab6e0e7edae75f1c2cbc8d5f
Douglas Saraiva <douglas.barbosa@zamp.com.br>
2023-06-01 15:18:49 -0300

    -

--- teste_function_http/gcloud_functions_deploy.sh
+++
@@ -1,9 +0,0 @@
-gcloud functions deploy teste_function_http \
-  --entry-point hello_world \
-  --runtime python311 \
-  --region us-east1 \
-  --trigger-http \
-  --memory '128' \
-  --timeout '30s' \
-  --min-instances 0 \
-  --max-instances 1000
